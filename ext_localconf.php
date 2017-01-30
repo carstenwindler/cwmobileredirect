2a5be2496796af50f8050a069e9fbdd22c97bf62
Online Now! GmbH <typo3@online-now.de>
2017-01-30 10:23:24 +0100

    Fix availability of user functions in newer TYPO3 versions

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,3 +9,6 @@ $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/index_ts.php']['preprocessRequest'][]
 
 $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['configArrayPostProc']['tx_cwmobileredirect']
     = 'EXT:cwmobileredirect/Classes/Main.php:&CarstenWindler\\Cwmobileredirect\\Main->secondEntryPoint';
+	
+// include user functions to use in typoscript conditions	
+require_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('cwmobileredirect') . 'Classes/UserFunctions.php');
